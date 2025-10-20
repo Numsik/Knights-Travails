7320548e66166991d0b55c998586d5149942ad77
Numsik <honzik.klusek.myto@gmail.com>
2025-10-20 15:02:12 +0200

    v3

--- index.js
+++ index.js
@@ -18,6 +18,37 @@ function findIndex(array, target){
     }
 }
 
+
+function findnextmove(index, x, y){
+    if (index == 0) return [x+2, y+1]
+    if (index == 1) return [x+1, y+2]
+    if (index == 2) return [x-1, y+2]
+    if (index == 3) return [x-2, y+1]
+    if (index == 4) return [x-2, y-1]
+    if (index == 5) return [x-1, y-2]
+    if (index == 6) return [x+1, y-2]
+    if (index == 7) return [x+2, y-1]
+}
+
+function buildadjlist(board){
+    let adjlist = []
+    for (let i =0; i < board.length; i++){
+        let neighbourds = [];
+        for (let j = 0;j < 8; j++){
+            let neighbourd = findnextmove(j,board[i][0],board[i][1]);
+            if (containsSpot(board, neighbor)){
+                neighbourds.push(findIndex(board, neighbourd))
+            }
+        }
+        adjlist[i] = neighbourds;
+    }
+    return adjlist;
+}
+
 function knightMoves(start, end){
+    let board = buildboard();
+    let startindex = findIndex(board, start);
+    let endindex = findIndex(board, end)
+
 
 };
