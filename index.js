71d3ea692413b8af728913f04d07ba145cb856bd
Numsik <honzik.klusek.myto@gmail.com>
2025-10-21 09:13:13 +0200

    almost done

--- index.js
+++ index.js
@@ -30,7 +30,19 @@ function findnextmove(index, x, y){
     if (index == 7) return [x+2, y-1]
 }
 
-function buildadjlist(board){
+function buildInfoArr(boardArr, startIndex) {
+    let newArr = [];
+    for (let i=0; i < boardArr.length; i++) {
+        newArr[i] = {
+            distance: null,
+            predecessor: null
+        }
+    }
+    newArr[startIndex].distance = 0;
+    return newArr;
+}
+
+function buildAdjList(board){
     let adjlist = []
     for (let i =0; i < board.length; i++){
         let neighbourds = [];
@@ -45,10 +57,44 @@ function buildadjlist(board){
     return adjlist;
 }
 
+
+function constructPath(board, infoarr, item, index, newarr){
+}
+
 function knightMoves(start, end){
     let board = buildboard();
     let startindex = findIndex(board, start);
-    let endindex = findIndex(board, end)
+    let endindex = findIndex(board, end);
+    let bfsinfo = buildInfoArr(board, startindex)
+    let adjlist = buildAdjList(board)
+    let queue = [startindex]
+    let u;
+    while (u != endindex){
+        u = queue.shift()
+
+        for(let i = 0; i < adjlist[u].length; i++){
+            let vIndex = adjlist[u][i];
+
+            if (vIndex === endindex){
+
+                bfsinfo[vIndex].predecessor = u;
+                let path = []
+                constructPath(board, bfsinfo, bfsinfo[vIndex], vIndex, path);
+                result = path.reverse().splice(0,0,start);
+                console.log(`you made it in ${path.length - 1} moves, your path:`)
+                return path;
+
+            }else{
+                if(bfsinfo[vIndex].distance == null){
+                    bfsinfo[index].distance = bfsinfo[u].distance + 1;
+                    bfsinfo[vIndex].predecessor = u;
+                    queue.push(vIndex);
+                }
+            }
+        }
+    }
 
 
 };
+
+console.log(knightMoves([0,0],[5,5]))
